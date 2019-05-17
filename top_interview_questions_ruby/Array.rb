9547433942af1ddcdf1f41ca33c24a3162be7172
ScottDenton <scottyd_14@hotmail.com>
2019-05-17 16:50:02 -0700

    rotate/ all array questions complete

--- top_interview_questions_ruby/Array.rb
+++ top_interview_questions_ruby/Array.rb
@@ -139,3 +139,27 @@ end
 #   [".",".","4",".",".",".",".",".","."]])
 
 # ***************  END OF SUDOKU
+# *********ROTATE IMAGE ***********
+
+#  thank the ruby docs for this one.
+def rotate(matrix)
+  matrix.replace(matrix.reverse.transpose)
+end
+
+# p rotate([
+#   [1,2,3],
+#   [4,5,6],
+#   [7,8,9]
+# ],)
+
+
+
+
+
+
+
+
+
+
+
+#
