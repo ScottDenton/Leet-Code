8f034705a01d955f80aab43cb1860019ed69b92b
ScottDenton <scottyd_14@hotmail.com>
2019-05-19 11:32:05 -0700

    first unique character in string

--- top_interview_questions_ruby/Strings.rb
+++ top_interview_questions_ruby/Strings.rb
@@ -23,3 +23,24 @@ def reverse(x)
     return converted.bit_length < 32 ? converted : 0
 end
 # p reverse(-123)
+
+# ************   FIRST UNIQUE CHARACTER IN STRING  *************
+# slow solution - beat 25% for runtime
+def first_uniq_char(s)
+  hash = {}
+  for i in s.split('')
+     hash.include?(i) ? hash[i] += 1 :hash[i] = 1
+  end
+  hash.each {|key, value| return s.index(key) if value == 1}
+  -1
+end
+# faster solution beat 91% for runtime
+def first_uniq_char(s)
+  hash = Hash.new(0)
+  s.chars.each{|char| hash[char] += 1}
+  hash.each {|key, value| return s.index(key) if value == 1}
+  -1
+end
+
+
+p first_uniq_char("loveleetcode")
