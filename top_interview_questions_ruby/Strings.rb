c80bc9e8e09e301a28b6b7415e364b21d074f12f
ScottDenton <scottyd_14@hotmail.com>
2019-05-20 12:24:11 -0700

    String to integer

--- top_interview_questions_ruby/Strings.rb
+++ top_interview_questions_ruby/Strings.rb
@@ -25,6 +25,7 @@ end
 # p reverse(-123)
 
 # ************   FIRST UNIQUE CHARACTER IN STRING  *************
+
 # slow solution - beat 25% for runtime
 def first_uniq_char(s)
   hash = {}
@@ -34,7 +35,8 @@ def first_uniq_char(s)
   hash.each {|key, value| return s.index(key) if value == 1}
   -1
 end
-# faster solution beat 91% for runtime
+
+# faster solution - beat 91% for runtime
 def first_uniq_char(s)
   hash = Hash.new(0)
   s.chars.each{|char| hash[char] += 1}
@@ -42,5 +44,64 @@ def first_uniq_char(s)
   -1
 end
 
+# p first_uniq_char("loveleetcode")
 
-p first_uniq_char("loveleetcode")
+
+
+# *****************    VALID ANAGRAM    **************************
+
+# not very efficient but simple solution
+def is_anagram(s, t)
+   s.chars.sort == t.chars.sort
+end
+
+# p is_anagram("anagram", "nagaram")
+
+
+
+# **************     VALID PALINDROME **************
+
+def is_palindrome(s)
+     s.downcase.gsub(/\W/,'') == s.downcase.gsub(/\W/,'').reverse
+end
+
+# p is_palindrome("A man, a plan, a canal: Panama")
+
+
+# ********     STRING TO INTEGER (atoi) *****************
+
+def my_atoi(str)
+  num = str.to_i
+    if num > 2147483647
+      return 2147483647
+    elsif num < -2147483648
+        return -2147483648
+    else
+      return num
+    end
+end
+
+#  regex mess that didnt work if there were more than one space at the start
+# def my_atoi(str)
+#   if str.match(/^(\s?[0-9]|\s?\+\d).*?$/)
+#     num = str.gsub(/\+/,'')
+#     num = num.match(/(\s?\d*)/)[0].to_i
+#     if num > 2147483648
+#       return 2147483648
+#     else
+#       return num
+#     end
+#   elsif str.match(/^(\s?[0-9]|\s?\-).*$/)
+#     num = str.match(/(\s?-\d*)/)[0].to_i
+#     if num < -2147483648
+#       return -2147483648
+#     else
+#       return num
+#     end
+#   else
+#     return 0
+#   end
+# end
+
+
+p my_atoi("   +42 dsaf")
