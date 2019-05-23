dfd06d84acb2066b15092f7ba9c65f59bd49dd1a
ScottDenton <scottyd_14@hotmail.com>
2019-05-23 09:46:54 -0700

    rotate array

--- top_interview_questions_javascript/Array.js
+++ top_interview_questions_javascript/Array.js
@@ -43,6 +43,23 @@ var maxProfit = function(prices) {
 // }
 
 
+// *****     ROTATE ARRAY     *****
+
+// option 1, average runtime and memory
+var rotate = function(nums, k) {
+  for(let i = 0; i < k; i++){
+    nums.unshift(nums.pop())
+  };
+};
+
+// option 2, leetcode does not recognise flat as a function
+var rotate = function(nums, k) {
+nums.unshift(nums.splice(nums.length-k))
+  return nums.flat()
+};
+
+console.log(rotate([1,2,3,4,5,6,7], 2))
+
 
 
 
