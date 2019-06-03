0937f7fc622d7007c8738914c1981a3a62417c3d
ScottDenton <scottyd_14@hotmail.com>
2019-06-03 08:55:06 -0700

    q 25 complete

--- projectEuler/questions21-30.rb
+++ projectEuler/questions21-30.rb
@@ -22,4 +22,51 @@ def sum_of_amicable(max)
   amicable_numbers(max).reduce(:+)
 end
 
-p sum_of_amicable(10000)
+# p sum_of_amicable(10000)
+
+# question 23
+# check if num is abundant
+def abundant_number?(num)
+  num < (1...num).select{|n| num % n == 0}.reduce(:+)
+end
+
+# find all abundant numbers below given number
+def abundant_below(num)
+  (12...(num/2 +1)).select{|n| abundant_number?(n)}
+end
+# p abundant_below(100)
+
+# check if the num can be made by summing to abundant numbers less than it
+def sum_of_abundant?(num)
+  array = abundant_below(num)
+  !array.select{|n| array.include?(num -n) }.empty?
+end
+
+# p sum_of_abundant?(100)
+
+def non_abundant_integers(max)
+  (12..max).select{|n| !sum_of_abundant?(n)}
+end
+
+# p non_abundant_integers(100).reduce(:+)
+
+# **** WORKS BUT IS SUPER SLOW
+
+# QUESTION: 25
+# 100 digit fib
+
+# find 1000 digit number and set as upper limit
+#  i is set as a counter / shows the current index
+max  = 10 ** 999
+a = 1
+b = 1
+i = 2
+
+while b < max
+  i += 1
+  temp = a
+  a = b
+  b = a + temp
+end
+
+puts i
